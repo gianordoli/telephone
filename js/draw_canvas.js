0d34895d314862a51fc11f751941c1d40f74361d
gianordoli <gianordoligabriel@yahoo.com.br>
2014-03-15 15:28:48 -0400

    Drawing connections

--- js/draw_canvas.js
+++ js/draw_canvas.js
@@ -16,15 +16,19 @@ window.requestAnimFrame = (function(callback) {
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 var canvasPosition;
-var marginTop;
+var canvasImages;
 var margin;
 /*---------------------------------------------*/
 
 function setup(){
-  marginTop = $('title').height();
-  console.log('margin top: ' + marginTop);
-  margin = 20;
   canvasResize();
+  canvasImages = [];
+  margin = 20;  
+  for(var i = 0; i < allImages.length; i++){
+    var img = new Object();  //creating object
+    initImage(img, i, allResults[i], allImages[i]);      //initializing
+    canvasImages.push(img);
+  }
   update();
 }
 
@@ -32,43 +36,66 @@ function update(){
   draw();
 }
 
-function draw(result){
-  console.log('called draw');
+function draw(){
+  // console.log('called draw');
   //Erasing the background
-  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = 'black';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // ctx.fillStyle = 'black';
+  // ctx.fillRect(0, 0, canvas.width, canvas.height);
   // ctx.beginPath();
   // ctx.arc(Math.random()*canvas.width, Math.random()*canvas.height,
   //         40, 40, 0, Math.PI*2, false);
   // ctx.fill();  
   
-  for(var i = 0; i < allImages.length; i++){
-    var img = allImages[i];
-    console.log('image ' + i + ": width = " + img.width);
-    var pos = new Object();
-    if(i == 0){
-      pos = {x: margin,
-             y: margin}
-    }else if(i == allImages.length - 1){
-      pos = {x: canvas.width - img.width - margin,
-             y: canvas.height - img.height - margin}
-    }else{
-      pos = {x: Math.random()*canvas.width - img.width - margin,
-             y: Math.random()*canvas.height - img.height - margin}
+  for(var i = 0; i < canvasImages.length; i++){
+    var obj = canvasImages[i];
+    if(i < canvasImages.length - 1){
+      var next = canvasImages[i + 1];
+      ctx.strokeStyle = 'black';
+      ctx.beginPath();
+      ctx.moveTo(obj.pos.x, obj.pos.y);
+      ctx.lineTo(next.pos.x, next.pos.y);
+      ctx.stroke();
     }
-    ctx.drawImage(img, pos.x, pos.y);
+
+    // console.log(obj);
+    ctx.fillStyle = 'white';
+    ctx.drawImage(obj.img, obj.pos.x, obj.pos.y);
   }
 
-  // request = requestAnimFrame(update);   
+  request = requestAnimFrame(update);   
+}
+
+function initImage(obj, _index, _result, _img){
+  var index = _index;
+  var result = _result;
+  var img = _img;
+  var pos = new Object();
+  if(_index == 0){
+    pos = {x: margin,
+           y: margin}
+  }else if(_index == allImages.length - 1){
+    pos = {x: canvas.width - img.width - margin,
+           y: canvas.height - img.height - margin}
+  }else{
+    pos = {x: margin + Math.random() * (canvas.width - img.width - margin),
+           y: margin + Math.random() * (canvas.height - img.height - margin) }
+  }  
+
+  obj.result = result;
+  obj.img = img;
+  obj.pos = pos;
 }
 
 function canvasResize(){
   screenWidth = window.innerWidth;
   screenHeight = window.innerHeight;
 
+  marginTop = $('#title').height();
+  // console.log('margin top: ' + marginTop);
+
   canvasPosition = canvas.getBoundingClientRect(); // Gets the canvas position
-  canvas.width = screenWidth - 4;
-  canvas.height = screenHeight - 4;
+  canvas.width = screenWidth - 10;
+  canvas.height = screenHeight - marginTop - 10;
 } 
